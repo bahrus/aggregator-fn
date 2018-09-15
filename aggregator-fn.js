280dd3a0bec9438da9fec7799f489f51e11a2ee7
bahrus <anderson.bruce.b@gmail.com>
2018-09-15 16:06:21 -0400

    ...

--- aggregator-fn.js
+++ aggregator-fn.js
@@ -1,46 +1,47 @@
-import {XtallatX} from 'xtal-latx/xtal-latx.js';
-import {define} from 'xtal-latx/define.js';
-import {destruct, getScript} from 'xtal-latx/destruct.js';
+import { XtallatX } from 'xtal-latx/xtal-latx.js';
+import { define } from 'xtal-latx/define.js';
+import { destruct, getScript } from 'xtal-latx/destruct.js';
 const input = 'input';
-export class AggregatorFn extends XtallatX(HTMLElement){
-    static get is() { return 'aggregator-fn';}
+export class AggregatorFn extends XtallatX(HTMLElement) {
+    constructor() {
+        super(...arguments);
+        this._aggregator = null;
+    }
+    static get is() { return 'aggregator-fn'; }
     static get observedAttributes() {
         return super.observedAttributes.concat([input]);
     }
-    _input: any | null;
-    get input(){
+    get input() {
         return this._input;
         this.aggregate();
     }
-    set input(val){
+    set input(val) {
         this._input = val;
         this.aggregate();
     }
-    _value: any | null;
-    get value(){
+    get value() {
         return this._value;
     }
-    set value(val){
+    set value(val) {
         this._value = val;
         this.de('value', {
             value: val
-        })
+        });
     }
-    _aggregator: ((input: any) => any) | null = null;
-    get aggregator(){
+    get aggregator() {
         return this._aggregator;
     }
-    set aggregator(val){
+    set aggregator(val) {
         this._aggregator = val;
         this.aggregate();
-        
     }
-    aggregate(){
-        if(this._input === undefined || this._aggregator === undefined || this._aggregator === null) return;
-        this.value = this._aggregator(this._input); 
+    aggregate() {
+        if (this._input === undefined || this._aggregator === undefined || this._aggregator === null)
+            return;
+        this.value = this._aggregator(this._input);
     }
-    attributeChangedCallback(name: string, oldVal: string, newVal: string) {
-        switch(name){
+    attributeChangedCallback(name, oldVal, newVal) {
+        switch (name) {
             case input:
                 this.input = JSON.parse(newVal);
                 break;
@@ -49,13 +50,12 @@ export class AggregatorFn extends XtallatX(HTMLElement){
     }
     connectedCallback() {
         this.style.display = 'none';
-        this._upgradeProperties(['disabled', input])
+        this._upgradeProperties(['disabled', input]);
         this.getS();
     }
-    _script!: HTMLScriptElement;
-    getS(){
-        this._script = this.querySelector('script') as HTMLScriptElement;
-        if(!this._script){
+    getS() {
+        this._script = this.querySelector('script');
+        if (!this._script) {
             setTimeout(() => {
                 this.getS();
             }, 10);
@@ -63,10 +63,11 @@ export class AggregatorFn extends XtallatX(HTMLElement){
         }
         this.eval();
     }
-    eval(){
+    eval() {
         const sInf = getScript(this._script);
-        if(sInf === null) return;
-        sInf.args.forEach(arg =>{
+        if (sInf === null)
+            return;
+        sInf.args.forEach(arg => {
             destruct(this, arg);
         });
         const temp = eval(`({
@@ -79,3 +80,4 @@ export class AggregatorFn extends XtallatX(HTMLElement){
     }
 }
 define(AggregatorFn);
+//# sourceMappingURL=aggregator-fn.js.map
